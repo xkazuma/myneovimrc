75e66cb67195d271b091842ad0a97ff573dac79e
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2023-07-29 17:41:48 +0900

    enhanced code completion to add plugins luasnip and mason-tool-installer

--- installer/nodejs/install.sh
+++ installer/nodejs/install.sh
@@ -4,4 +4,12 @@ cd installer/nodejs \
   && wget https://nodejs.org/dist/v18.17.0/node-v18.17.0-linux-x64.tar.xz \
   && tar xf node-v18.17.0-linux-x64.tar.xz \
   && rm ./node-v18.17.0-linux-x64.tar.xz \
-  && ln -f node-v18.17.0-linux-x64/bin/node ~/.local/bin
+  && ln -f node-v18.17.0-linux-x64/bin/node ~/.local/bin \
+  && NPMPATH=$(find $(pwd)/node-v18.17.0-linux-x64/bin/ -name "npm") \
+  && NPXPATH=$(find $(pwd)/node-v18.17.0-linux-x64/bin/ -name "npx") \
+  && echo "#!/bin/bash" > bin/npm \
+  && echo "#!/bin/bash" > bin/npx \
+  && echo "${NPMPATH} \$*" >> bin/npm \
+  && echo "${NPXPATH} \$*" >> bin/npx \
+  && ln -f bin/npm ~/.local/bin \
+  && ln -f bin/npx ~/.local/bin
