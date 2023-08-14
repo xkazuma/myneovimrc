6a0780a3f7cb9b371ad62fd33fc2e1e9df5bdc80
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2023-08-14 10:18:50 +0900

    change timeoutlen 300 to 900

--- lua/options.lua
+++ lua/options.lua
@@ -1,15 +1,15 @@
 local options = {
-	encoding       = "utf-8",
-	fileencoding   = "utf-8",
+	encoding       = 'utf-8',
+	fileencoding   = 'utf-8',
 	title          = true,
 	backup         = false,
-	clipboard      = "unnamedplus",
+	clipboard      = 'unnamedplus',
 	cmdheight      = 2,
-	completeopt    = { "menuone", "noselect" },
+	completeopt    = { 'menuone', 'noselect' },
 	conceallevel   = 0,
 	hlsearch       = true,
 	ignorecase     = true,
-	mouse          = "",
+	mouse          = '',
 	pumheight      = 10,
 	showmode       = false,
 	showtabline    = 2,
@@ -17,37 +17,37 @@ local options = {
 	smartindent    = true,
 	swapfile       = false,
 	termguicolors  = true,
-	timeoutlen     = 300,
+	timeoutlen     = 900,
 	undofile       = true,
 	updatetime     = 400,
 	writebackup    = false,
-	shell          = "bash",
-	backupskip     = { "/tmp/*", "/private/tmp/*" },
+	shell          = 'bash',
+	backupskip     = { '/tmp/*', '/private/tmp/*' },
 	expandtab      = true,
 	shiftwidth     = 2,
 	tabstop        = 2,
 	cursorline     = true,
 	number         = true,
-	relativenumber = false,
+	relativenumber = true,
 	numberwidth    = 4,
-	signcolumn     = "yes",
-	wrap           = false,
+	signcolumn     = 'yes',
+	wrap           = true,
 	winblend       = 0,
-	wildoptions    = "pum",
+	wildoptions    = 'pum',
 	pumblend       = 5,
-	background     = "dark",
+	background     = 'dark',
 	scrolloff      = 8,
 	sidescrolloff  = 8,
-	guifont        = "monospace:h17",
+	guifont        = 'HackGen Console NF:h17',
 	splitbelow     = true,
 	splitright     = true,
 }
 
-vim.opt.shortmess:append("c")
+vim.opt.shortmess:append('c')
 
 for k, v in pairs(options) do
 	vim.opt[k] = v
 end
 
-vim.cmd("set whichwrap+=<,>,[,],h,l")
+vim.cmd('set whichwrap+=<,>,[,],h,l')
 vim.cmd([[set iskeyword+=-]])
