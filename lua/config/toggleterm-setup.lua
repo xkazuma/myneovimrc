fcf864f3ccfd4ac395f2d859d0530906f0b9eb43
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2023-08-20 04:47:20 +0900

    fix toggleterm-config

--- lua/config/toggleterm-setup.lua
+++ lua/config/toggleterm-setup.lua
@@ -2,14 +2,14 @@ local tt = require("toggleterm")
 
 tt.setup({
   -- size can be a number or function which is passed the current terminal
-  size = 20 | function(term)
+  size = function(term)
     if term.direction == "horizontal" then
       return 15
     elseif term.direction == "vertical" then
       return vim.o.columns * 0.4
     end
   end,
-  open_mapping = [[<c-\]],
+  open_mapping      = [[<c-\]],
   start_in_insert   = true,
   insert_mappings   = true, -- whether or not the open mapping applies in insert mode
   terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
