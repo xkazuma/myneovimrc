e2ddced79928371753f3b245c0bbc917fb05ec64
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2023-08-20 03:13:23 +0900

    update bufferline-config, add icons

--- lua/config/bufferline-setup.lua
+++ lua/config/bufferline-setup.lua
@@ -7,21 +7,15 @@ require('bufferline').setup({
     show_buffer_close_icons = false,
     show_close_icon         = false,
     color_icons             = true,
-    --- count is an integer representing total count of errors
-    --- level is a string "error" | "warning"
-    --- diagnostics_dict is a dictionary from error level ("error", "warning" or "info")to number of errors for each level.
-    --- this should return a string
-    --- Don't get too fancy as this function will be executed a lot
+    get_element_icon        = function(element)
+      local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
+      return icon, hl
+    end,
+    diagnostics             = "nvim_lsp",
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
       local icon = level:match("error") and " " or " "
       return " " .. icon .. count
     end,
-    --- SIDEBAR OFFSET                                           *bufferline-offset*
-    --- 
-    --- You can prevent the bufferline drawing above a *vertical* sidebar split such as a file explorer.
-    --- To do this you must set the `offsets` configuration option to a list of tables
-    --- containing the details of the window to avoid. *NOTE:* this is only relevant
-    --- for left or right aligned sidebar windows such as `NvimTree`, `NERDTree` or
     offsets = {
       {
         filetype  = "NvimTree",
