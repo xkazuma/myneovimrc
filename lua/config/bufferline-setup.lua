3d4764954601bc4a5a7f7f90c0b8a339306fd886
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2023-08-20 02:59:10 +0900

    change buffers from tabs in bufferline-config

--- lua/config/bufferline-setup.lua
+++ lua/config/bufferline-setup.lua
@@ -1,7 +1,7 @@
 vim.opt.termguicolors = true
 require('bufferline').setup({
   options = {
-    mode                    = "tabs",
+    mode                    = "buffers",
     separator_style         = 'slant',
     always_show_bufferline  = false,
     show_buffer_close_icons = false,
