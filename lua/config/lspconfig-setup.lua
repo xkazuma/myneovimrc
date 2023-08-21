4a9036f31202b367a9f50f54e013ae3950810a70
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2023-08-21 10:50:55 +0900

    fix nvim-navic config

--- lua/config/lspconfig-setup.lua
+++ lua/config/lspconfig-setup.lua
@@ -12,6 +12,13 @@ lspconfig.lua_ls.setup({
   }
 })
 
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
--- lua/config/mason-setup.lua
+++ lua/config/mason-setup.lua
@@ -1,13 +1,9 @@
-local on_attach = function(client, bufnr)
-end
-
 require('mason-lspconfig').setup_handlers {
   function(server_name)
     require('lspconfig')[server_name].setup {
       capabilities = require('cmp_nvim_lsp').update_capabilities(
         vim.lsp.protocol.make_client_capabilities()
       ),
-      on_attach = on_attach
     }
   end
 }
