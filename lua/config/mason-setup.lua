968a34a8d55f93fac9a71bc0d48360e8cf08e2e6
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2023-08-21 10:37:23 +0900

    fix nvim-navic config

--- lua/config/mason-setup.lua
+++ lua/config/mason-setup.lua
@@ -1,4 +1,5 @@
-local navic      = require('nvim-navic')
+local navic = require('nvim-navic')
+
 local on_attach = function(client, bufnr)
   if client.server_capabilities.documentSymbolProvider then
     navic.attach(client, bufnr)
@@ -8,20 +9,20 @@ end
 require('mason-lspconfig').setup_handlers {
   function(server_name)
     require('lspconfig')[server_name].setup {
-      on_attach    = on_attach,
       capabilities = require('cmp_nvim_lsp').update_capabilities(
         vim.lsp.protocol.make_client_capabilities()
       ),
+      on_attach = on_attach
     }
   end
 }
 
 require("mason").setup({
-    ui = {
-        icons = {
-            package_installed   = "✓",
-            package_pending     = "➜",
-            package_uninstalled = "✗"
-        }
+  ui = {
+    icons = {
+      package_installed   = "✓",
+      package_pending     = "➜",
+      package_uninstalled = "✗"
     }
+  }
 })
