e99659094c7a8a82297cc6540e4c8672fc541182
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2023-08-21 12:05:29 +0900

    fix nvim-navic config

--- lua/config/lualine-setup.lua
+++ lua/config/lualine-setup.lua
@@ -54,4 +54,11 @@ require('lualine').setup {
   },
   tabline = {},
   extensions = {},
+  winbar = {
+    lualine_c = {
+      "navic",
+      color_correction = nil,
+      navic_opts = nil
+    }
+  }
 }
