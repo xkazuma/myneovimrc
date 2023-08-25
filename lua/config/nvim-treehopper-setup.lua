5b0154ce7c6837423457d9086e1855ff55ef4fba
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2023-08-25 15:30:51 +0900

    fix treehopper-config

--- lua/config/nvim-treehopper-setup.lua
+++ lua/config/nvim-treehopper-setup.lua
@@ -1,4 +1,4 @@
-local api        = api
+local vim        = vim
 local treehopper = require('tsht')
 
 treehopper.config.hint_keys = {
@@ -7,9 +7,12 @@ treehopper.config.hint_keys = {
   'r', 'a', 'n',
 }
 
-api.nvim_set_keymap('o',
+vim.api.nvim_set_keymap('o',
   '<leader>ht', '<cmd>lua require("tsht").nodes()<cr>',
   { noremap = false, silent = true })
-api.nvim_set_keymap({ 'n', 'x' },
+vim.api.nvim_set_keymap('n',
+  '<leader>ht', '<cmd>lua require("tsht").nodes()<cr>',
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x',
   '<leader>ht', '<cmd>lua require("tsht").nodes()<cr>',
   { noremap = true, silent = true })
