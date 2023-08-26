ca54cc62aa2ab16b152e283a5c6a769478987c08
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2023-08-26 16:53:29 +0900

    setup keybinds of comment-box

--- lua/config/comment-box-setup.lua
+++ lua/config/comment-box-setup.lua
@@ -1,18 +1,9 @@
 local keymap = vim.keymap.set
 local cb     = require('comment-box')
 
--- left aligned fixed size box with left aligned text
-keymap({'n', 'v'}, '<leader>cbb', cb.lbox, {})
--- centered adapted box with centered text
-keymap({'n', 'v'}, '<leader>cbc', cb.ccbox, {})
-
--- centered line
-keymap('n', '<leader>cbl', cb.cline, {})
-keymap('i', '<M-l>',       cb.cline, {})
-
 cb.setup({
   doc_width = 80, -- width of the document
-  box_width = 60, -- width of the boxes
+  box_width = 70, -- width of the boxes
   borders   = {   -- symbols used to draw a box
     top          = "─",
     bottom       = "─",
@@ -30,7 +21,38 @@ cb.setup({
     line_end   = "─",
   },
   outer_blank_lines     = false, -- insert a blank line above and below the box
-  inner_blank_lines     = false, -- insert a blank line above and below the text
+  inner_blank_lines     = true,  -- insert a blank line above and below the text
   line_blank_line_above = false, -- insert a blank line above the line
-  line_blank_line_below = false, -- insert a blank line below the line
+  line_blank_line_below = true,  -- insert a blank line below the line
 })
+
+-- open catalog
+keymap('n',        '<leader>cbc',   cb.catalog,  {})
+
+-- comment box
+local boxn = 22
+for i=1,boxn do
+  keymap({'n', 'v'}, '<leader>cbll' .. string.format('%02d', i), cb.llbox(i),  {})
+  keymap({'n', 'v'}, '<leader>cblc' .. string.format('%02d', i), cb.lcbox(i),  {})
+  keymap({'n', 'v'}, '<leader>cblr' .. string.format('%02d', i), cb.lrbox(i),  {})
+  keymap({'n', 'v'}, '<leader>cbcl' .. string.format('%02d', i), cb.clbox(i),  {})
+  keymap({'n', 'v'}, '<leader>cbcc' .. string.format('%02d', i), cb.ccbox(i),  {})
+  keymap({'n', 'v'}, '<leader>cbcr' .. string.format('%02d', i), cb.crbox(i),  {})
+  keymap({'n', 'v'}, '<leader>cbrl' .. string.format('%02d', i), cb.rlbox(i),  {})
+  keymap({'n', 'v'}, '<leader>cbrc' .. string.format('%02d', i), cb.rcbox(i),  {})
+  keymap({'n', 'v'}, '<leader>cbrr' .. string.format('%02d', i), cb.rrbox(i),  {})
+  keymap({'n', 'v'}, '<leader>cbal' .. string.format('%02d', i), cb.albox(i),  {})
+  keymap({'n', 'v'}, '<leader>cbac' .. string.format('%02d', i), cb.acbox(i),  {})
+  keymap({'n', 'v'}, '<leader>cbar' .. string.format('%02d', i), cb.arbox(i),  {})
+end
+
+-- comment line
+local linen = 10
+for i=1,linen do
+  keymap('n', '<leader>cbll' .. string.format('%02d', i), cb.lline(i),  {})
+  keymap('n', '<leader>cblc' .. string.format('%02d', i), cb.cline(i),  {})
+  keymap('n', '<leader>cblr' .. string.format('%02d', i), cb.rline(i),  {})
+  keymap('i', '<M-l>l', cb.lline, {})
+  keymap('i', '<M-l>c', cb.cline, {})
+  keymap('i', '<M-l>r', cb.rline, {})
+end
