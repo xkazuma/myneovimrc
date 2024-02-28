951bd08b62dd838ee59ec3081465146b089300f5
Kazuma Kusu <kusu@mil.doshisha.ac.jp>
2024-02-29 01:08:38 +0900

    indent-blankline: version up 2 to 3

--- lua/config/indent-blankline-setup.lua
+++ lua/config/indent-blankline-setup.lua
@@ -1,20 +1,30 @@
-local ib = require('indent_blankline')
+local ibl = require('ibl')
+local vim = vim
 
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#363422 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#463422 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#564432 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#665442 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#766452 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#867462 gui=nocombine]]
-
-ib.setup {
-    space_char_blankline = ' ',
-    char_highlight_list  = {
-        'IndentBlanklineIndent1',
-        'IndentBlanklineIndent2',
-        'IndentBlanklineIndent3',
-        'IndentBlanklineIndent4',
-        'IndentBlanklineIndent5',
-        'IndentBlanklineIndent6',
-    },
+local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
 }
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed",    { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue",   { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen",  { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan",   { fg = "#56B6C2" })
+end)
+
+
+vim.g.rainbow_delimiters = { highlight = highlight }
+require("ibl").setup { scope = { highlight = highlight } }
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
